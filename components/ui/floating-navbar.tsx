379c65430b217925b2b0986275d70c29419e05c2
Ariel Agdon <arielagdon@yahoo.com>
2025-06-24 22:57:03 +0800

    disable darkmode

--- components/ui/floating-navbar.tsx
+++ components/ui/floating-navbar.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import { cn } from "@/lib/utils";
 import Link from "next/link";
-import { ModeToggle } from "../mode-toggle";
+// import { ModeToggle } from "../mode-toggle";
 import { useMediaQuery } from "usehooks-ts";
 
 interface NavItem {
